d55dc4233f5fb74c4a80ae896552643de37167d9
Artem Lodygin <aldaer@yandex.ru>
2016-10-18 02:18:47 +0300

    Tuple2 created

---
+++ src/test/java/EdxTasksPart1.java
@@ -0,0 +1,11 @@
+import org.junit.Test;
+
+public class EdxTasksPart1 {
+    @Test
+    public void testTupleRDD1() throws Exception {
+        scala.Tuple2<String, Integer> person1 = new scala.Tuple2<>("Alice", 11);
+        scala.Tuple2<String, Integer> person2 = new scala.Tuple2<>("Bob", 13);
+
+
+    }
+}
